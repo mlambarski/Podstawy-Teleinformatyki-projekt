4ce21f687e01f48010608777d1497ae32eb3696f
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-03-21 19:01:04 +0100

    face detection

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import cv2
+import sys
 
+imagePath = "9.jpg"
+cascPath = "haarcascade_frontalface_default.xml"
 
-print(cv2.__version__)
+faceCascade = cv2.CascadeClassifier(cascPath)
 
-img = cv2.imread('C:/Users/Ela/Desktop/A&A&N v2/50.jpg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('image', img)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    flags=cv2.CASCADE_SCALE_IMAGE
+)
+
+print "Found {0} faces!".format(len(faces))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found" ,image)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
