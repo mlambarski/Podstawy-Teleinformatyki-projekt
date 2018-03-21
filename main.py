a7811c6647bb146328d8f6ffe50b453cdace07cf
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-03-21 18:27:26 +0100

    first step in openCV

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import numpy
-import sys
+import cv2
 
-print("lol")
+
+print(cv2.__version__)
+
+img = cv2.imread('C:/Users/Ela/Desktop/A&A&N v2/50.jpg', cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
